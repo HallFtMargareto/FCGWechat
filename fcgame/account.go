4c363881cbbead02afe0b8d26ec32fc96fd7c371
xusc <xusc@outlook.com>
2025-10-28 00:06:37 +0800

    使用密钥解密指定文件

---
+++ fcgame/account.go
@@ -0,0 +1,24 @@
+package fcgame
+
+/*
+{Name: "wxid_7t9azqe51qyg22_0210", SortName: "", Platform: "windows", Version: 4, FullVersion: "4.0.3.36", DataDir: "C:\\Users\\oliver\\Documents\\xwechat_files\\wxid_7t9azqe51qyg22_0210", Key: "3520aff0dafe4f83b6d255c073e3f7afc8b6d316123d44c1b587b3f07d30099c", ImgKey: "34303365613865303735336430636332", PID: 4052, ExePath: "C:\\Program Files\\Tencent\\Weixin\\Weixin.exe", Status: "online", SavedAt: 1757837104}
+*/
+
+func FetchAccount() AccountInfo {
+	account := AccountInfo{
+		Name:        "wxid_7t9azqe51qyg22_0210",
+		SortName:    "",
+		Platform:    "windows",
+		Version:     4,
+		FullVersion: "4.1.2.17",
+		DataDir:     "C:\\Users\\oliver\\Documents\\xwechat_files\\wxid_7t9azqe51qyg22_021",
+		Key:         "3520aff0dafe4f83b6d255c073e3f7afc8b6d316123d44c1b587b3f07d30099c",
+		ImgKey:      "34303365613865303735336430636332",
+		PID:         1000,
+		ExePath:     "C:\\Program Files\\Tencent\\Weixin\\Weixin.exe",
+		Status:      "online",
+		SavedAt:     1757837104,
+	}
+
+	return account
+}
